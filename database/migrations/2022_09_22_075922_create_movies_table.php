e444dd1090b3fd4421fc6a20f977527a72af916f
Kévin Kowalsky <kevin.kowalsky1@gmail.com>
2022-09-22 10:16:19 +0200

    created migrations

---
+++ database/migrations/2022_09_22_075922_create_movies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('synopsys');
+            $table->integer('duration');
+            $table->string('youtube')->nullable();
+            $table->string('cover');
+            $table->date('released_at')->nullable();
+            $table->foreignId('category_id')->nullable()->constrained();
+            $table->foreignId('user_id')->nullable()->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+};
